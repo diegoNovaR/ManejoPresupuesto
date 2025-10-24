f0cc8c6fcb4f0d3f774ea0dfbc03273ee3333892
Diego Nova <diegonova368@gmail.com>
2025-10-24 09:38:27 -0500

    Agregando formulario con validaciones

--- DesarrolloManejoPresupuesto/ManejoPresupuesto/ManejoPresupuesto/Controllers/TiposCuentasController.cs
+++ DesarrolloManejoPresupuesto/ManejoPresupuesto/ManejoPresupuesto/Controllers/TiposCuentasController.cs
@@ -1,11 +1,24 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ManejoPresupuesto.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace ManejoPresupuesto.Controllers
 {
-    public class TiposCuentasController:Controller 
+    public class TiposCuentasController : Controller
     {
-        public IActionResult Crear() {
+        public IActionResult Crear()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Crear(TipoCuenta tipoCuenta)
+        {
+            if(!ModelState.IsValid)// si el modelo no es valido
+            {
+                return View(tipoCuenta);// con esto estamos devolviendo el modelo con los errores de validacion
+            }
             return View();
         }
     }
+
 }
