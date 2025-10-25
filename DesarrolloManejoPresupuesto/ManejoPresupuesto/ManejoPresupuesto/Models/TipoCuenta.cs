7fd7c652194954c0732612eecfe6f27d45f346f2
Diego Nova <diegonova368@gmail.com>
2025-10-25 10:15:14 -0500

    Agregando la validación

--- DesarrolloManejoPresupuesto/ManejoPresupuesto/ManejoPresupuesto/Models/TipoCuenta.cs
+++ DesarrolloManejoPresupuesto/ManejoPresupuesto/ManejoPresupuesto/Models/TipoCuenta.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using ManejoPresupuesto.Validaciones;
+using System.ComponentModel.DataAnnotations;
 
 namespace ManejoPresupuesto.Models
 {
@@ -7,6 +8,7 @@ namespace ManejoPresupuesto.Models
         public int Id { get; set; }
         [Required(ErrorMessage = "El campo {0} es obligatorio")]
         [StringLength(maximumLength:50, MinimumLength = 3, ErrorMessage = "La longitud del campo {0} debe estar entre {2} y {1}")]
+        [PrimeraLetraMayuscula]
         public string Nombre { get; set; }
         public int UsuarioId { get; set; }
         public int Orden { get; set; }
