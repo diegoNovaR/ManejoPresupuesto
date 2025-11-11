16b4ac1a7ec4dee9fb74186fb6120c134a9895dd
Diego Nova <diegonova368@gmail.com>
2025-11-11 09:47:16 -0500

    Agregando vista para crear cuenta, y query para crear el mismo.

--- DesarrolloManejoPresupuesto/ManejoPresupuesto/ManejoPresupuesto/Controllers/CuentasController.cs
+++ DesarrolloManejoPresupuesto/ManejoPresupuesto/ManejoPresupuesto/Controllers/CuentasController.cs
@@ -18,6 +18,20 @@ namespace ManejoPresupuesto.Controllers
             this.repositorioCuentas = repositorioCuentas;
         }
 
+
+        public async Task<IActionResult> Index()
+        {
+            var usuarioId = servicioUsuarios.ObtenerUsuarioId();
+            var cuentasConTipoCuenta = await repositorioCuentas.Buscar(usuarioId);
+            var modelo = cuentasConTipoCuenta.GroupBy(x => x.TipoCuenta)
+                .Select( grupo => new IndiceCuentasViewModel{
+                    TipoCuenta = grupo.Key,
+                    Cuentas = grupo.AsEnumerable(),
+                }).ToList();
+            return View(modelo);
+        }
+
+
         [HttpGet]
         public async Task<IActionResult> Crear()
         {
