82cf2e51f9fe939b18c74234799a041a443db33b
Diego Nova <diegonova368@gmail.com>
2025-11-11 09:07:49 -0500

    Agregando modelo cuenta, vista cuenta y servicio. Configurando INSERTAR CUENTA

---
+++ DesarrolloManejoPresupuesto/ManejoPresupuesto/ManejoPresupuesto/Models/CuentaCreacionViewModel.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace ManejoPresupuesto.Models
+{
+    public class CuentaCreacionViewModel:Cuenta
+    {
+        //nos permite ccrear selectionables en las vistas
+        public IEnumerable<SelectListItem> TiposCuentas { get; set; }//selectListItem es una clase que representa un elemento en un dropdown
+
+    }
+}
